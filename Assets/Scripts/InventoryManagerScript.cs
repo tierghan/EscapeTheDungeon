306364a73ad568a7a4bdb347327e7eba5b3e33a6
tierghan <50425512+tierghan@users.noreply.github.com>
2026-03-31 16:10:06 -0400

    v0.0.5
    Decided to cut the inventory and item systems save for consumables. I don't have the knowledge and time to implement it well enough to be in the final game.

--- Assets/Scripts/InventoryManagerScript.cs
+++ Assets/Scripts/InventoryManagerScript.cs
@@ -8,11 +8,29 @@ public class InventoryManagerScript : MonoBehaviour, IDataPersistance
     GameObject player;
     [SerializeField] private TextMeshProUGUI consumableText;
 
+    List<string> inventory = new List<string>();
+
+    GameObject itemInfoManager;
+    TMP_Text inventorySlot1Name, inventorySlot1Desc, inventorySlot2Name, inventorySlot2Desc, inventorySlot3Name, inventorySlot3Desc, inventorySlot4Name, inventorySlot4Desc;
+
+    ScriptableObject itemSO;
+
+
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        itemInfoManager = GameObject.Find("ItemInfoManager");
         UpdateConsumableText();
+
+        inventorySlot1Name = GameObject.Find("InventorySlot1ItemName").GetComponent<TextMeshProUGUI>();
+        inventorySlot1Desc = GameObject.Find("InventorySlot1ItemDesc").GetComponent<TextMeshProUGUI>();
+        inventorySlot2Name = GameObject.Find("InventorySlot2ItemName").GetComponent<TextMeshProUGUI>();
+        inventorySlot2Desc = GameObject.Find("InventorySlot2ItemDesc").GetComponent<TextMeshProUGUI>();
+        inventorySlot3Name = GameObject.Find("InventorySlot3ItemName").GetComponent<TextMeshProUGUI>();
+        inventorySlot3Desc = GameObject.Find("InventorySlot3ItemDesc").GetComponent<TextMeshProUGUI>();
+        inventorySlot4Name = GameObject.Find("InventorySlot4ItemName").GetComponent<TextMeshProUGUI>();
+        inventorySlot4Desc = GameObject.Find("InventorySlot4ItemDesc").GetComponent<TextMeshProUGUI>();
     }
 
     public void UsePotion()
@@ -57,8 +75,47 @@ public class InventoryManagerScript : MonoBehaviour, IDataPersistance
 
 
 
-    
+    public void AddItem(string itemName)
+    {
+        if(inventory.Count < 5)
+        {
+            inventory.Add(itemName);
+        }
+    }
 
+    public void DisplayItemInSlot(int slotIndex)
+    {
+        switch (slotIndex)
+        {   
+            //Slot 1
+            case 0:
+                itemSO = itemInfoManager.GetComponent<ItemInfoManager>().GetScriptableObject(inventory[0]);
+                inventorySlot1Name.text = itemSO.itemName;
+                inventorySlot1Desc.text = itemSO.description;
+                break;
+            //Slot 2
+            case 1:
+                itemSO = itemInfoManager.GetComponent<ItemInfoManager>().GetScriptableObject(inventory[1]);
+                inventorySlot2Name.text = itemSO.itemName;
+                inventorySlot2Desc.text = itemSO.description;
+                break;
+            //Slot 3
+            case 2:
+                itemSO = itemInfoManager.GetComponent<ItemInfoManager>().GetScriptableObject(inventory[2]);
+                inventorySlot3Name.text = itemSO.itemName;
+                inventorySlot3Desc.text = itemSO.description;
+                break;
+            //Slot 4
+            case 3:
+                itemSO = itemInfoManager.GetComponent<ItemInfoManager>().GetScriptableObject(inventory[3]);
+                inventorySlot4Name.text = itemSO.itemName;
+                inventorySlot4Desc.text = itemSO.description;
+                break;
+            default:
+                Debug.Log("Invalid inventory slot index. Tried to display item in slot index: " + slotIndex);
+                break;
+        }
+    }
 
 
 
--- Assets/Scripts/ItemInfoManager.cs
+++ Assets/Scripts/ItemInfoManager.cs
@@ -6,4 +6,161 @@ public class ItemInfoManager : MonoBehaviour
 {
     // This script is to let the inventory system fetch item info from item tierID and typeID. This should return each items
     // scriptable object from a given tier and type. Thanks to this, only the tier and type need to be saved when saving the game.
+
+    // Type Map:
+    // 0 | Weapon
+    // 1 | Armor
+    // 2 | Spellbook
+
+
+    public string GetItemName(int itemTier, int itemType)
+    {
+        switch (itemType)
+        {
+            //weapon
+            case 0:
+                return GetWeaponName(itemTier);
+                
+            //armor
+            case 1:
+                return GetArmorName(itemTier);
+
+            //spellbook
+            case 2:
+                return GetSpellbookName(itemTier);
+
+            //test item
+            case 10:
+                return "TestItem";
+
+            //default
+            default:
+                Debug.Log("Invalid item type.");
+                return "Error: Invalid Item Type";
+        }
+    }
+
+    public int GetItemTier(string itemName)
+    {
+        if (itemName.Contains("Broken") || itemName.Contains("Leather") || itemName.Contains("Healing"))
+        {
+            return 0;
+        }
+        else if (itemName.Contains("Copper") || itemName.Contains("Hardhide") || itemName.Contains("Fireball"))
+        {
+            return 1;
+        }
+        else if (itemName.Contains("Iron") || itemName.Contains("Chainmail") || itemName.Contains("Earthquake"))
+        {
+            return 2;
+        }
+        else if (itemName.Contains("Silver") || itemName.Contains("Plate") || itemName.Contains("Ice Spike"))
+        {
+            return 3;
+        }
+        else if (itemName.Contains("Sacred") || itemName.Contains("Paladin") || itemName.Contains("Protection"))
+        {
+            return 4;
+        }
+        else if (itemName.Contains("TestItem"))
+        {
+            return 10;
+        }
+        else
+        {
+            Debug.Log("Invalid item name.");
+            return -1;
+        }
+    }
+
+    public int GetItemType(string itemName)
+    {
+        if (itemName.Contains("Sword"))
+        {
+            return 0;
+        }
+        else if (itemName.Contains("Armor"))
+        {
+            return 1;
+        }
+        else if (itemName.Contains("Spellbook"))
+        {
+            return 2;
+        }
+        else if (itemName.Contains("TestItem"))
+        {
+            return 10;
+        }
+        else
+        {
+            Debug.Log("Invalid item name.");
+            return -1;
+        }
+    }
+
+    private string GetWeaponName(int itemTier)
+    {
+        switch (itemTier)
+        {
+            case 0:
+                return "Broken Sword";
+            case 1:
+                return "Copper Sword";
+            case 2:
+                return "Iron Sword";
+            case 3:
+                return "Silver Sword";
+            case 4:
+                return "Sacred Sword";
+            default:
+                Debug.Log("Invalid item tier.");
+                return "Error: Invalid Item Tier";
+        }
+    }
+
+    private string GetArmorName(int itemTier)
+    {
+        switch (itemTier)
+        {
+            case 0:
+                return "Leather Armor";
+            case 1:
+                return "Hardhide Armor";
+            case 2:
+                return "Chainmail Armor";
+            case 3:
+                return "Plate Armor";
+            case 4:
+                return "Paladin Armor";
+            default:
+                Debug.Log("Invalid item tier.");
+                return "Error: Invalid Item Tier";
+        }
+    }
+
+    private string GetSpellbookName(int itemTier)
+    {
+        switch (itemTier)
+        {
+            case 0:
+                return "Healing Spellbook";
+            case 1:
+                return "Fireball Spellbook";
+            case 2:
+                return "Earthquake Spellbook";
+            case 3:
+                return "Ice Spike Spellbook";
+            case 4:
+                return "Protection Spellbook";
+            default:
+                Debug.Log("Invalid item tier.");
+                return "Error: Invalid Item Tier";
+        }
+    }
+
+    public ScriptableObject GetScriptableObject(string itemName)
+    {
+        ScriptableObject itemSO = Resources.Load<ScriptableObject>("Items/" + itemName);
+        return itemSO;
+    }
 }
